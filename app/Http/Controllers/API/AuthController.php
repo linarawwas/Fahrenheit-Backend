9f19261658bf548ac68ca3ce3777959aabd2b1e5
linarawwas <linamkraw2@gmail.com>
2023-07-20 11:18:18 +0200

    updates

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -9,18 +9,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 use Illuminate\Validation\ValidationException;
 use Carbon\Carbon;
 
 class AuthController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:sanctum', ['except' => ['login', 'register']]);
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:sanctum', ['except' => ['login', 'register']]);
+    // }
 
     public function login(Request $request)
     {
+        log::info("ZEINABBBBBBBBBBB");
         $request->validate([
             'username' => 'required|string',
             'password' => 'required|string',
